a9d7b487d50cdf5c19cec46c5c2c71b0260d6948
Sarah-Specialist <92344379+Sarah-Specialist@users.noreply.github.com>
2021-12-17 21:41:59 +0800

    rearranging

---
+++ index.js
@@ -0,0 +1,7 @@
+var e = document.getElementById('icon');
+e.onmouseover = function() {
+document.getElementById('information').style.display = 'block';
+}
+e.onmouseout = function() {
+document.getElementById('information').style.display = 'none';
+}
