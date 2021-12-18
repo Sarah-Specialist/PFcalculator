de25cde33519d9b51421f50f5f37efde8bdb6f8c
Sarah-Specialist <92344379+Sarah-Specialist@users.noreply.github.com>
2021-12-18 09:36:10 +0800

    attempted device function

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Division of PaO2 and FiO2 inputs, and validation
 
 
-function division (event) {
+function division(event) {
     event.preventDefault();
 
     let numberFormat = /^[0-9]+$/;;
@@ -22,6 +22,39 @@ function division (event) {
     }
 }
 
+// radio buttons to FiO2input
+function device() {
+    let selectedInput = document.getElementById("FiO2");
+    
+    if (np1.checked==true){
+        let np1 = document.getElementById("np1");
+        selectedInput.innerText = `0.25`;
+    }
+    if (np2.checked==true){
+        const np2 = document.getElementById("np2");
+        selectedInput.innerText = `0.29`;
+    }
+    if (np3.checked==true){
+        const np3 = document.getElementById("np3");
+        selectedInput.innerText = `0.33`;
+    }
+    if (np4.checked==true){
+        const np4 = document.getElementById("np4");
+        selectedInput.innerText = `0.37`;
+    }
+    if (np5.checked==true){
+        const np5 = document.getElementById("np5");
+        selectedInput.innerText = `0.41`;
+    }
+    if (np6.checked==true){
+        const np6 = document.getElementById("np6");
+        selectedInput.innerText = `0.45`;
+    }
+    if (nrm.checked==true){
+        const nrm = document.getElementById("nrm");
+        selectedInput.innerText = `1`;
+    }
+}
 
 
 // Mouseover & Mouseout Icon
