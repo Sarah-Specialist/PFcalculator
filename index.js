acd28673ea13aa05276f6da9adabff8d1f8d17c5
Sarah-Specialist <92344379+Sarah-Specialist@users.noreply.github.com>
2021-12-18 08:46:30 +0800

    added division function

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
+// Division of PaO2 and FiO2 inputs, and validation
+
+
+function division (event) {
+    event.preventDefault();
+
+    let numberFormat = /^[0-9]+$/;;
+    let PaO2input = document.getElementById("PaO2").value;
+    console.log(PaO2input);
+    let FiO2input = document.getElementById("FiO2").value;
+    console.log(FiO2input);
+    if (PaO2input.match(numberFormat) && FiO2input>0 && FiO2input<=1) {
+        let result = PaO2input/FiO2input;
+        console.log(result);
+        document.querySelector("span").innerText = `Patient's PaO2/FiO2 ratio is: ${result}`;
+    } else if (!PaO2input.match(numberFormat) && FiO2input>0 && FiO2input<=1) {
+        alert("PaO2 must be a number");
+    } else if (PaO2input.match(numberFormat) && (FiO2input>1 || !FiO2input)) {
+        alert("Please input FiO2 as decimal e.g. 50% = 0.5");
+    } else {
+        alert(`PaO2 must be a number \nPlease input FiO2 as decimal e.g. 50% = 0.5`);
+    }
+}
+
 
 
 // Mouseover & Mouseout Icon
